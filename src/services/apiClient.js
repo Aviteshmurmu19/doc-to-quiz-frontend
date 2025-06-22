06d8dc7c539440c3677e1b74af5831ab0c572680
Avitesh Murmu <Aviteshmurmu19@gmail.com>
2025-06-23 00:44:32 +0530

    updated api client

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,7 +3,7 @@
 // const API_URL = "http://localhost:3001/api/generate";
 // This now works for both local development (with a proxy) and production
 const BASE_URL = import.meta.env.VITE_SOME_KEY || "http://localhost:3001";
-console.log("BASE_URL", BASE_URL);
+// console.log("BASE_URL", BASE_URL);
 const API_URL = `${BASE_URL}/api/generate`;
 
 export const generateQuiz = async (textContent) => {
